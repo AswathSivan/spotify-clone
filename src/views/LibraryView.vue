fec0b63e356f63ce2cd244f3a59461227bdac145
aswath.sp <aswath.sp@bridge-global.com>
2024-03-12 17:51:41 +0530

    library view changes

--- src/views/LibraryView.vue
+++ src/views/LibraryView.vue
@@ -1,16 +1,32 @@
 <template>
     <div class="p-8 overflow-x-hidden">
-        <button class="text-white font-semibold text-3xl hover:underline underline-offset-auto cursor-pointer"
+        <button class="text-white font-semibold text-2xl hover:underline underline-offset-auto "
             type="button">
             {{ artist.name }}
         </button>
         <div class="py-2"></div>
         <div class="w-full h-full flex items-center relative">
-            <img width="180" :src="artist.albumCover">
-            <div>
-                <div class="text-white absolute w-full hover:underline cursor-pointer top-0 font-semibold">{{ artist.name }}</div>
+            <img width="160" :src="artist.albumCover" class="rounded-lg">
+            <div class="ml-5 w-full">
+                <div class="text-white absolute w-full hover:underline cursor-pointer top-0 text-5xl font-semibold">{{ artist.name }}</div>
+                <div class="text-gray-300 text-sm flex font-semibold">
+                    <div class="flex">Album . {{ artist.releaseYear }} . {{ artist.tracks.length }} </div>
+                </div>
+                <div class="absolute flex items-center justify-start bottom-0 mb-2 gap-4">
+                    <button class="p-1 rounded-full bg-white">
+                        <Play v-if="!isPlaying" fillColor="#181818" :size="25"/>
+                        <Pause v-else fillColor="#181818" :size="25"/>
+                    </button>         
+                    <button type="button">
+                        <Heart fillColor="#1BD760" :size="30"/>
+                    </button>
+                    <button type="button">
+                        <DotsHorizontal fillColor="#FFFFFF" :size="25"/>
+                    </button>   
+                </div>
             </div>
         </div>
+
     </div>
 
 </template>
@@ -23,4 +39,6 @@ import ClockTimeThreeOutline from 'vue-material-design-icons/ClockTimeThreeOutli
 // import artist from "./artist.json";
 import artist from '../songs.json'
 import SongRow from '../components/SongRow.vue'
+import { ref } from 'vue';
+const isPlaying = ref(false)
 </script>
